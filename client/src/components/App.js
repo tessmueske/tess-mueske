cb0ffd03767a058ac308ef62eaa7d933501cbe8c
Tess <tess.mueske@gmail.com>
2024-11-12 20:57:18 +0100

    fonts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,12 +5,12 @@ import About from "./About"
 function App() {
   return (
     <Router>
-    <div>
+    <div className="center-container">
       <h1>welcome to tess mueske's portfolio</h1>
       <a href="https://my-period-app.onrender.com/" target="_blank" rel="noopener noreferrer">
         crimson, a non-gendered period tracking application
       </a>
-      <p>crimson fills a gap in the menstruation tracking app space. the apps on the market right now use gendered language, either for bodies or for products. this project does not.
+      <p>crimson fills a gap in the menstruation tracking app space. the apps on the market use gendered language, either for bodies or for products. this project does not.
 
         crimson uses a Javascript + React frontend with a fully functional backend incorporating Python with Flask and Flask-SQLAlchemy with a RESTful API routing. it uses Formik for form validations, and it's deployed using render. for secure authentication, the project uses Bcrypt for password hashing.</p>
         <Link to="/about">about tess</Link>
