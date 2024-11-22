9ae6516b3c6dc87969fc41a11c3182c35ece1141
Tess <tess.mueske@gmail.com>
2024-11-22 20:31:10 +0100

    decaps

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -3,14 +3,16 @@ import { Switch, Route, Link } from "react-router-dom";
 
 function Projects(){
     return (
-        <div className="center-container">
+        <div className="project-container">
+            <h1>projects</h1>
+            <p>i have experience with javascript, react, html, css, python, flask, sqlalchemy, and render for cloud deployment. i've worked with orms, full crud, restful routing, json-based data structures, and flask apis. i have experience with formik for form validations on the frontend, bcrypt for password hashing, flask mail extension for account verification, and formspree for contact forms.</p>
             <h1> crimson </h1>
             <a href="https://my-period-app.onrender.com/" target="_blank" rel="noopener noreferrer">
                 check it out here.
             </a>
             <p>crimson, a non-gendered period tracking application. crimson fills a gap in the menstruation tracking app space. the apps on the market use gendered language, either for bodies or for products. this project does not.
                 
-            crimson uses a Javascript + React frontend with a fully functional backend, incorporating Python with Flask and Flask-SQLAlchemy and RESTful API routing. it uses Formik for form validations, and it's deployed using render. for secure authentication, the project uses Bcrypt for password hashing.</p>
+            crimson uses a javascript + react frontend with a fully functional backend, incorporating python with flask and flask-sqlalchemy and restful api routing. it uses form for form validations, and it's deployed using render. for secure authentication, the project uses bcrypt for password hashing.</p>
 
             <h1> magwa </h1>
             <p>a travel app for the canary islands focusing on sustainable tourism and local businesses. coming soon.</p>
