0b4df42ded43825d39b49136c1babe1b3dfbc0f6
Tess <tess.mueske@gmail.com>
2024-12-14 16:15:00 +0100

    magwa addition

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -15,7 +15,10 @@ function Projects(){
             crimson uses a javascript + react frontend with a fully functional backend, incorporating python with flask and flask-sqlalchemy and restful api routing. it uses form for form validations, and it's deployed using render. for secure authentication, the project uses bcrypt for password hashing.</p>
 
             <h1> magwa </h1>
-            <p>a travel app for the canary islands focusing on sustainable tourism and local businesses. coming soon.</p>
+            <p>a travel app for the canary islands focusing on sustainable tourism and local businesses. built with javascript + react with a python + flask backend, using flask sessions for session management, flask mail extension for account verification, hashtag filtering, 5 python models, over 20 client- and server- side routes.</p>
+            <a href="https://github.com/tessmueske/tourism-app" target="_blank" rel="noopener noreferrer">
+                check out the github repo here.
+            </a>
             <Link to="/">home</Link>
         </div>
     )
